0715ab4c663fba33776b00dcbf98d11d75dbf260
Emil Akhmedov <emil282@ukr.net>
2023-03-04 18:49:40 +0200

    bug fix script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let numOrStr = prompt('input number or string');
 console.log(numOrStr);
 
-switch (numOrStr) {
-    case null:
+switch (true) {
+    case (numOrStr === null):
         console.log('ви скасували');
         break; 
-    case '':
+    case (numOrStr.trim() === ''):
         console.log('Empty String');
         break; 
-    case isNaN( +numOrStr ):
+    case isNaN( +numOrStr):
         console.log('Number is Ba_NaN');
         break;     
     default:
